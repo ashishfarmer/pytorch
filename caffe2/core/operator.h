f412c252c7b258ec20040bfc76f5c49204b4c652
ashishfarmer <ashish.farmer@amd.com>
2020-09-15 22:57:24 +0000

    aliad CUDATensor to HIPTensor on ROCm at Caffe2 input

--- caffe2/core/operator.h
+++ caffe2/core/operator.h
@@ -210,8 +210,16 @@ class CAFFE2_API OperatorBase : public Observable<OperatorBase> {
       t = t.contiguous();
     }
     Tensor tensor = caffe2::Tensor(std::move(t));
-    CAFFE_ENFORCE_EQ(tensor.GetDeviceType(), type);
-    input_tensors_[idx] = std::move(tensor);
+    if (type == DeviceType::HIP && (tensor.GetDeviceType() == DeviceType::CUDA)) {
+      Tensor hip_aliased_tensor = caffe2::Tensor(DeviceType::HIP);
+      hip_aliased_tensor.ResizeLike(tensor);
+      hip_aliased_tensor.ShareExternalPointer(tensor.raw_mutable_data(), tensor.dtype(), tensor.numel() * tensor.itemsize());
+      input_tensors_[idx] = std::move(hip_aliased_tensor);
+    }
+    else {
+      CAFFE_ENFORCE_EQ(tensor.GetDeviceType(), type);
+      input_tensors_[idx] = std::move(tensor);
+    }
     return input_tensors_[idx];
 #else
     CAFFE_THROW("Non-legacy operators are not legal in xplat/caffe2");
